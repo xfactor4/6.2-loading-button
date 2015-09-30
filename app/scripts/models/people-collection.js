99056c6be0947307b54ec7ce7b7968428e2bc9eb
xfactor4 <gdrobinson92@comcast.net>
2015-09-29 21:34:18 -0400

    Update 2015-09-30T01:34:16.611Z

--- app/scripts/models/people-collection.js
+++
@@ -1,8 +0,0 @@
-import Person from 'models/person';
-
-var PeopleCollection = Backbone.Collection.extend({
-  model: Person,
-  url: "http://tiny-lasagna-server.herokuapp.com/collections/things"
-});
-
-export default PeopleCollection;
